b982775526297808da3d210e46225ab5886891af
Coding Club JNTUHCEH <76844183+Coding-Club-JNTUHCEH@users.noreply.github.com>
2023-11-08 23:12:28 +0530

    Que2.cpp

---
+++ Contest-1/C++/Que2.cpp
@@ -0,0 +1,14 @@
+// Approach -> Given, the positions of two hunters and a rabbit. We need to find absolute value of distance b/w hunters and rabbit, and compare them.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x, y, z;
+    cin >> x >> y >> z;
+    int a = abs(x-z);
+    int b = abs(y-z);
+    if (a == b) cout << 'C';
+    else if (a > b) cout << 'B';
+    else    cout << 'A';
+}
