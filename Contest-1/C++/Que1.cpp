d72f198b6878b8153627e6d99e232f7cf4559732
Coding Club JNTUHCEH <76844183+Coding-Club-JNTUHCEH@users.noreply.github.com>
2023-11-08 22:36:01 +0530

    Create Que1.cpp

---
+++ Contest-1/C++/Que1.cpp
@@ -0,0 +1,11 @@
+// Approach -> We need to print the total number of chocolates present i.e the SUM of n and m
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    cout << n+m << endl;
+    return 0;
+}
