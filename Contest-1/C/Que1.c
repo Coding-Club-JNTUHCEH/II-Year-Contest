ed9c822f6e54fad957f681eb42d55cef75526b95
Coding Club JNTUHCEH <76844183+Coding-Club-JNTUHCEH@users.noreply.github.com>
2023-11-08 22:40:07 +0530

    Create Que1.c

---
+++ Contest-1/C/Que1.c
@@ -0,0 +1,10 @@
+// Approach -> We need to print the total number of chocolates present i.e the SUM of n and m
+
+#include <stdio.h>
+
+int main() {
+  int n, m;
+  scanf("%d %d", &n, &m);
+  printf("%d\n", n + m);
+  return 0;
+}
